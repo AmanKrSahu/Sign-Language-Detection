2d154b9974448f8ff1fe4258628fb6e487b84d77
Aman <amankrsahu003@gmail.com>
2024-10-07 15:48:35 +0530

    Added Project Requirements

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="Sign-Language-Detection",
+    version="0.0.1",
+    author="Aman Kumar Sahu",
+    author_email="amankrsahu003@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
